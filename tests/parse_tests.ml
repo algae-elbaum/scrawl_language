e00fa563c9cfa4ea20acf71045b1a923613bae1e
helbaum <helbaum@caltech.edu>
2016-04-26 07:13:41 +0000

    The parse test is now more correct
    Still need proper error reporting and to eliminate remaining ambiguities in the
    grammar

--- tests/parse_tests.ml
+++ tests/parse_tests.ml
@@ -1,8 +1,20 @@
 
 let parse_ok_test () =
-    let lexbuf = Lexing.from_channel stdin in
-    let result = Parser.main Lexer.tokenize lexbuf in
-    true
+    Printf.printf "Parsing a file with no errors.\n";
+    let file = open_in "tests/parse_test.sc" in
+    let try_wrap () =
+        try
+            let lexbuf = Lexing.from_channel file in
+            let result = Parser.main Lexer.tokenize lexbuf in
+            Printf.printf "Successfully parsed\n\n";
+            close_in file;
+            true
+        with 
+            | _ -> (* TODO 0: get error reporting *)
+                  Printf.printf "Got an error, no parsing error reporting yet\n\n";
+                  close_in file;
+                  false
+    in try_wrap ()
 
 let parse_bad_test () = true
 
