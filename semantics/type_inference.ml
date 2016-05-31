cda48e90f99218700f5e30af3e44842ad194c956
helbaum <helbaum@caltech.edu>
2016-05-30 22:38:14 -0700

    Function types are no longer to be inferred

--- semantics/type_inference.ml
+++
@@ -1,4 +0,0 @@
-(** The functions in this file take care of determining types of things not
-    explicitly given. For now, and probably forever, that refers only to
-    determining the return type of functions *)
-
