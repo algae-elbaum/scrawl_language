51cfa8d583fbf948df091d2da95425cd15d2c78f
helbaum <helbaum@caltech.edu>
2016-06-07 00:34:59 -0700

    Restruct code to better match expr-stm distinction

--- semantics/intermediate_tree.ml
+++ semantics/intermediate_tree.ml
@@ -4,7 +4,7 @@
     between the AST and machine code. In practice since we are running out of
     time, this will the the final form of the code, and we'll make an
     an interpreter that can run it *)
-type intermediate_tree = INTRM_TREE of expr list
+type intermediate_tree = INTRM_TREE of expr
 
 and expr =
     | I_CONST of int
@@ -23,23 +23,8 @@ and expr =
         same as (TEMP t). The idea is that (MEM_TEMP (PLUS (MEM t) e)) should be able to
         act exactly like a temp for the memory slot e slots down from (MEM t). This should
         work even with multiple layers, ie (MEM_TEMP (MEM (MEM_TEMP (MEM t)))) should still
-        act just the same as (TEMP t). Another case that should work is that after
-        MOVE(a, MEM (t)), (MEM_TEMP a) should also act like (TEMP t). This is for arrays to
-        work nicely. *) 
+        act just the same as (TEMP t). This is for arrays to work nicely. *) 
     | MEM_TEMP of expr
-    (*  The int in ALLOC_MEM is the number of words the temp will need if written to memory.
-        I'm having trouble understanding how the book wants to do this, since the book doesn't
-        have an ALLOC_MEM. The book seems to maintain in parallel a frame data structure. This
-        here feels simpler and clearer, especially since at this point we're aiming for having
-        an interpreter for this tree, rather than compiling all the way to x86. The idea for
-        this is that the interpreter would maintain an environment similar to the ones maintained
-        here. Upon ALLOC_MEM it would make some storage somehow, and the environment would map
-        temps to their storage. (Compiling to x86 would do essentially the same thing and probably
-        have an easier time of it) *)
-    | ALLOC_MEM of temp * int
-    (* (UNALLOC_MEM n) signals that the last n words of memory are now out of scope and should
-       be unallocated *)
-    | UNALLOC_MEM of int
     (* CALL of func * args. I think the func should only ever be a NAME or a TEMP, but TODO make sure *)
     | CALL of expr * expr list
     | ESEQ of stm * expr
@@ -60,6 +45,20 @@ and stm =
     | SEQ of stm * stm
     (* A label declared a label that may be JUMPed or CJUMPed to later *)
     | LABEL of label
+    (*  The int in ALLOC_MEM is the number of words the temp will need if written to memory.
+        I'm having trouble understanding how the book wants to do this, since the book doesn't
+        have an ALLOC_MEM. The book seems to maintain in parallel a frame data structure. This
+        here feels simpler and clearer, especially since at this point we're aiming for having
+        an interpreter for this tree, rather than compiling all the way to x86. The idea for
+        this is that the interpreter would maintain an environment similar to the ones maintained
+        here. Upon ALLOC_MEM it would make some storage somehow, and the environment would map
+        temps to their storage. (Compiling to x86 would do essentially the same thing and probably
+        have an easier time of it) *)
+    | ALLOC_MEM of temp * int
+    (* (UNALLOC_MEM n) signals that the last n words of memory are now out of scope and should
+       be unallocated *)
+    | UNALLOC_MEM of int
+
 
 and binop = 
     | BAND | BOR | BXOR | BLEFT | BRIGHT 
@@ -109,12 +108,17 @@ let int_of_bool b =
     if b then 1 else 0
 
 (** Add a new ident to the environments *)
-let add_ident ident ident_type loc_env type_env del =
+let add_temp_ident ident ident_type temp_env type_env del =
     let n_temp = new_temp () in
-    Hashtbl.add !loc_env ident n_temp;
+    Hashtbl.add !temp_env ident n_temp;
     Hashtbl.add !type_env ident ident_type;
     Stack.push ident !del;
     n_temp
+
+let add_label_ident ident lab_env =
+    let n_lab = new_label () in
+    Hashtbl.add !lab_env ident n_lab;
+    n_lab
 
 
 (* Turn a list of stms into a SEQ *)
@@ -131,31 +135,26 @@ let rec seq lst =
     | h::t -> SEQ (h, lst_to_seq t)
 
 let rec intermediate_of_ast (Abstract_syntax.AST tree) =
-    (* We'll maintain a mutating loc_env type_environment and pass it around by reference to
-     avoid the cost of copying the hash table all the time. A deletion stack
-     follows it around and records what must be deleted when leaving an 
-     loc_env type_environment. *)
-    let loc_env = ref (Hashtbl.create 10) in
+    (* We'll maintain mutating temp_env, lab_env, and type_env environments
+       and pass them around by reference. A deletion stack follows them around
+       and records what must be deleted when leaving a scope. *)
+    let temp_env = ref (Hashtbl.create 10) in
+    let lab_env = ref (Hashtbl.create 10) in
     let type_env = ref (Hashtbl.create 10) in
     let del = ref (Stack.create ()) in
-    INTRM_TREE (translate_ExprList tree loc_env type_env del);
+    INTRM_TREE (ESEQ ((translate_block tree [] false temp_env lab_env type_env del), I_CONST 1))
 
-and translate_ExprList expr_list loc_env type_env del = 
-    List.map (fun (e) -> translate_Expr e loc_env type_env del) expr_list
-
-and translate_Expr exp loc_env type_env del =
+and translate_Expr_expr exp temp_env lab_env type_env del =
     match exp with
-    | Abstract_syntax.VarExpr var -> translate_varExpr var loc_env type_env del
-    | Abstract_syntax.DeclExpr decl -> translate_declExpr decl loc_env type_env del
-    | Abstract_syntax.AssignExpr {var; value; pos} ->  I_CONST 1
+    | Abstract_syntax.VarExpr var -> translate_varExpr var temp_env lab_env type_env del
+    | Abstract_syntax.AssignExpr {var; value; pos} ->  I_CONST 1 (* TODO *)
     | Abstract_syntax.LambdaExpr {func_type; params; body; _} ->
         let f_start = new_label () in
-        let t_body = translate_block body params true loc_env type_env del in
+        let t_body = translate_block body params true temp_env lab_env type_env del in
         ESEQ (seq [LABEL f_start;
                    t_body], 
               NAME f_start)
 
-    | Abstract_syntax.ReturnExpr x -> I_CONST 1
     | Abstract_syntax.IntLitExpr {value; _} -> I_CONST value
     | Abstract_syntax.FloatLitExpr {value; _} -> F_CONST value
     (* Strings are not supported yet and will not be supported by the end of term *)
@@ -164,8 +163,8 @@ and translate_Expr exp loc_env type_env del =
     | Abstract_syntax.FuncCallExpr {func; args; _} -> I_CONST 1 (* TODO *)
     | Abstract_syntax.BinOpExpr {op; argl; argr; _} -> 
         begin
-            let (t_argl, t_argr) = (translate_Expr argl loc_env type_env del,
-                                    translate_Expr argr loc_env type_env del) in
+            let (t_argl, t_argr) = (translate_Expr_expr argl temp_env lab_env type_env del,
+                                    translate_Expr_expr argr temp_env lab_env type_env del) in
             match op with
             |Abstract_syntax.BAND -> BINOP(BAND, t_argr, t_argl)
             |Abstract_syntax.BOR -> BINOP (BOR, t_argr, t_argl)
@@ -188,61 +187,64 @@ and translate_Expr exp loc_env type_env del =
             |Abstract_syntax.LESS -> rel_expr LT t_argl t_argr
             |Abstract_syntax.GREATER -> rel_expr GT t_argl t_argr
         end
-
     | Abstract_syntax.UnOpExpr {op; arg; _} ->
         begin
-            let t_arg = translate_Expr arg loc_env type_env del in
+            let t_arg = translate_Expr_expr arg temp_env lab_env type_env del in
             match op with
             | Abstract_syntax.BNOT -> BINOP (BXOR, I_CONST 1, t_arg)
             (* LNOT works since translation turns binary values into 0 or 1 *)
             | Abstract_syntax.LNOT -> rel_expr EQ (I_CONST 0) t_arg
             | Abstract_syntax.UMINUS -> BINOP (MINUS, I_CONST 0, t_arg)
         end
+    | Abstract_syntax.NoOp -> I_CONST 1
+    | _ -> raise (Invalid_argument "I don't think this should ever happen, but I'm not 100%")
 
+and translate_Expr_stm exp temp_env lab_env type_env del =
+    match exp with
+    | Abstract_syntax.DeclExpr decl -> translate_declExpr decl temp_env lab_env type_env del
+    | Abstract_syntax.ReturnExpr x -> EXP (I_CONST 1) (* TODO *)
     | Abstract_syntax.IfExpr {cond; body; else_expr; _} ->
-        let t_cond = translate_Expr cond loc_env type_env del in
-        let t_body = translate_block body [] false loc_env type_env del in
-        let t_else = translate_block else_expr [] false loc_env type_env del in
+        let t_cond = translate_Expr_expr cond temp_env lab_env type_env del in
+        let t_body = translate_block body [] false temp_env lab_env type_env del in
+        let t_else = translate_block else_expr [] false temp_env lab_env type_env del in
         let t = new_label () in
         let f = new_label () in
         let e = new_label () in
-        ESEQ ((seq [CJUMP (EQ, I_CONST 1, t_cond, t, f);
-                    LABEL t;
-                    t_body;
-                    JUMP (NAME e);
-                    LABEL f;
-                    t_else;
-                    LABEL e;]),
-             (* This next line is a hack to get ifs to act like exprs. Might work out a better solution *)
-             (I_CONST 1))
+        seq [CJUMP (EQ, I_CONST 1, t_cond, t, f);
+             LABEL t;
+             t_body;
+             JUMP (NAME e);
+             LABEL f;
+             t_else;
+             LABEL e;]
     | Abstract_syntax.ForExpr _  -> 
         raise (Invalid_argument "Translator got AST with for loop (compiler bug)")
     | Abstract_syntax.WhileExpr {cond; body; preface; _} ->
-        let t_preface = translate_Expr preface loc_env type_env del in
-        let t_cond = translate_Expr cond loc_env type_env del in
-        let t_body = translate_block body [] false loc_env type_env del in
+        let t_preface = translate_Expr_expr preface temp_env lab_env type_env del in
+        let t_cond = translate_Expr_expr cond temp_env lab_env type_env del in
+        let t_body = translate_block body [] false temp_env lab_env type_env del in
         let c = new_label () in
         let e = new_label () in
-        ESEQ ((seq [EXP t_preface;
-                    CJUMP (EQ, I_CONST 1, t_cond, c, e);
-                    LABEL c;
-                    t_body;
-                    CJUMP (EQ, I_CONST 1, t_cond, c, e);
-                    LABEL e;]),
-             (* This next line is a hack to get whiles to act like exprs. Might work out a better solution *)
-             (I_CONST 1))
+        seq [EXP t_preface;
+             CJUMP (EQ, I_CONST 1, t_cond, c, e);
+             LABEL c;
+             t_body;
+             CJUMP (EQ, I_CONST 1, t_cond, c, e);
+             LABEL e;]
+    | _ -> raise (Invalid_argument "I don't think this should ever happen... TODO think this through better")
 
-    | Abstract_syntax.NoOp -> I_CONST 1
-
-and translate_varExpr var loc_env type_env del =
+and translate_varExpr var temp_env lab_env type_env del =
     match var with
-    | Abstract_syntax.SimpleVar {ident; _} -> TEMP (Hashtbl.find !loc_env ident) (* Return the associated temp *)
+    | Abstract_syntax.SimpleVar {ident; _} -> 
+        if Hashtbl.mem !temp_env ident
+            then TEMP (Hashtbl.find !temp_env ident) (* Return the associated TEMP *)
+            else NAME (Hashtbl.find !lab_env ident) (* Return the associated NAME *)
     | Abstract_syntax.ArrayVar {arr; idx; _} ->
         let arr_type = Hashtbl.find !type_env arr in
         match arr_type with
         | Abstract_syntax.ScrawlArrayType {array_type; len; _} ->
-            let trans_idx = translate_Expr idx loc_env type_env del in
-            let arr_temp = Hashtbl.find !loc_env arr in
+            let trans_idx = translate_Expr_expr idx temp_env lab_env type_env del in
+            let arr_temp = Hashtbl.find !temp_env arr in
             (* Prevent array out of bounds access: *)
             let in_bound = new_label () in
             let maybe_in_bound = new_label () in
@@ -261,33 +263,32 @@ and translate_varExpr var loc_env type_env del =
                   (MEM_TEMP (BINOP (PLUS, (MEM arr_temp), trans_idx))))
         | _ -> raise (Invalid_argument "This should be impossible")
 
-and translate_declExpr decl loc_env type_env del =
+and translate_declExpr decl temp_env lab_env type_env del =
     match decl with
     (* For simple decls we just make a new temp for the variable  *)
     | Abstract_syntax.SimpleDecl {var_type; ident; pos} ->
-        let tmp = add_ident ident var_type loc_env type_env del in
+        let tmp = add_temp_ident ident var_type temp_env type_env del in
         ALLOC_MEM (tmp, 1)
     | Abstract_syntax.ArrDecl {arr_type; ident; pos} ->
         (* !!!Arrays are only single dimensional!!! *)
         begin
-            let tmp = add_ident ident arr_type loc_env type_env del in
+            let tmp = add_temp_ident ident arr_type temp_env type_env del in
             match arr_type with
                 | Abstract_syntax.ScrawlArrayType {array_type; len; _} -> ALLOC_MEM (tmp, len)
                 | _ -> raise (Invalid_argument "This should be impossible")
         end
     | Abstract_syntax.FuncDecl {func_type; ident; params; body; pos} ->
-        let f_start = new_label () in
-        let t_body = translate_block body params true loc_env type_env del in
-        ESEQ (seq [LABEL f_start;
-                   t_body], 
-              NAME f_start)
+        let f_start = add_label_ident ident lab_env in
+        let t_body = translate_block body params true temp_env lab_env type_env del in
+        seq [LABEL f_start;
+             t_body]
 
 and rel_expr op argl argr =
     let t = new_label () in
     let f = new_label () in
     let e = new_label () in
     let res = new_temp () in
-    ESEQ ((seq [EXP (ALLOC_MEM (res, 1));
+    ESEQ ((seq [ALLOC_MEM (res, 1);
                 CJUMP (op, argl, argr, t, f);
                 LABEL t;
                 MOVE (TEMP res, I_CONST 1);
@@ -297,32 +298,31 @@ and rel_expr op argl argr =
                 LABEL e;]),
           (TEMP res))
 
-and translate_block block args is_func loc_env type_env del =
+and translate_block block args is_func temp_env lab_env type_env del =
     begin
     (* Start a new scope *)
     Stack.push "*" !del; 
     (* fetch each arg from the arg register before executing *)
     let fetch_args = 
         seq (List.mapi (fun i (Abstract_syntax.QualIdent {ident_type; ident; _}) ->
-                                let n_temp = add_ident ident ident_type loc_env type_env del in
+                                let n_temp = add_temp_ident ident ident_type temp_env type_env del in
                                  COPY (n_temp, (MEM_TEMP (BINOP (PLUS, (MEM arg_temp), I_CONST i)))))
                        args)
     in
-    (* TODO *)
-    rewind_env loc_env type_env del;
+    rewind_env temp_env lab_env type_env del;
     LABEL 1
     end
 
-(** Rewind the loc_env type_environment to immediately before it entered the current level of
-    scoping. Raises Empty when at global scope. (So don't use it at global scope.
-    There should be no reason to do so) *)
-and rewind_env loc_env type_env del =
+(** Rewind the temp_env and type_env environments to immediately before it 
+    entered the current level of scoping. Raises Empty when at global scope.
+    (So don't use it at global scope. There should be no reason to do so) *)
+and rewind_env temp_env lab_env type_env del =
     match (Stack.pop !del) with
     | "*" -> () (* A star denotes the beginning of a new scope *)
     | s -> begin
-           Hashtbl.remove !loc_env s;
+           Hashtbl.remove !temp_env s;
            Hashtbl.remove !type_env s;
-           rewind_env loc_env type_env del;
+           rewind_env temp_env lab_env type_env del;
            end
 
 
